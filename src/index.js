6f4b438e8ba4db9466f2d92f795ec52bbaa38682
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-11-10 21:31:51 +0200

    Set up basic css reset and other css files to be properly configured

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import './css/reset.css';
+import './css/typography.css';
+import './css/layout.css';
+import './css/components.css';
 import './css/styles.css';
 import { logic } from './logic';
 logic.fetchWeather('london');
--- src/logic.js
+++ src/logic.js
@@ -2,12 +2,22 @@ export const logic = (() => {
   const apiKey = 'A58FRBCCTVYQN9LT7Q79Z67WZ';
 
   const fetchWeather = async (city) => {
-    const result = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`
-    );
+    try {
+      const result = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`
+      );
 
-    const data = await result.json();
-    console.log(data);
+      if (!result.ok) {
+        throw new Error('Fail');
+      }
+
+      const data = await result.json();
+      console.log(data.currentConditions.temp);
+
+      return { data };
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return { fetchWeather };
