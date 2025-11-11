9be4ca2e3776ae6ccfccf78373b04cf286c18e8d
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-11-12 01:30:05 +0200

    Finish widgets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import './css/reset.css';
-import './css/typography.css';
-import './css/layout.css';
-import './css/components.css';
 import './css/styles.css';
 import { logic } from './logic';
 logic.fetchWeather('london');
