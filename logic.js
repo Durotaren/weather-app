8f6822efb1acd44397163a3f2671ab384505b752
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-11-11 16:08:43 +0200

    Add assets like pictures and svgs

---
+++ logic.js
@@ -0,0 +1,15 @@
+const logic = (() => {
+  const apiKey = 'A58FRBCCTVYQN9LT7Q79Z67WZ';
+
+  const fetchWeather = async (city) => {
+    const result = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`
+    );
+    console.log(result);
+
+    const data = await result.json();
+    console.log(data);
+  };
+
+  return { fetchWeather };
+})();
