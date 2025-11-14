82716431da502172f839ff2aa7c41d4bcee81e1a
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-11-14 15:22:02 +0200

    Implement logic to update 5 day forecast

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,33 @@
 import { logic } from './logic';
 
-const dom = (() => {
+export const dom = (() => {
   const input = document.getElementById('search-input');
   const searchBtn = document.querySelector('.search-img');
   const dayContainers = document.querySelectorAll('.day-container');
   const tempDisplay = document.querySelector('.temp');
-  const todayRight = document.querySelector('.today-right-high');
-  const todayLeft = document.querySelector('.today-right-low');
+  const todayHigh = document.querySelector('.today-right-high');
+  const todayLow = document.querySelector('.today-right-low');
+
+  const updateDisplay = (data) => {
+    tempDisplay.textContent = `${data.currentTemp}°C`;
+    todayHigh.textContent = `${Math.round(logic.fahrenheitToC(data.fiveDays[0].tempmax))}°C`;
+    todayLow.textContent = `${Math.round(logic.fahrenheitToC(data.fiveDays[0].tempmin))}°C`;
+  };
+
+  const updateDayContainers = (data) => {
+    let counter = 0;
+    console.log(data);
+
+    dayContainers.forEach((container) => {
+      const date = new Date(data.fiveDays[counter].datetime);
+      const day = date.toLocaleDateString('en-US', { weekday: 'long' });
+      container.firstElementChild.textContent = day;
+      container.lastElementChild.textContent = `${Math.round(
+        logic.fahrenheitToC(data.fiveDays[counter].tempmax)
+      )}°C`;
+      counter++;
+    });
+  };
 
   searchBtn.addEventListener('click', () => {
     if (!input.value) {
@@ -15,9 +36,13 @@ const dom = (() => {
 
     let formatInput =
       input.value.charAt(0).toUpperCase() + input.value.slice(1).toLowerCase();
-    logic.fetchWeather(formatInput);
+    logic.fetchWeather(formatInput).then((data) => {
+      (updateDisplay(data), updateDayContainers(data));
+    });
     input.value = '';
   });
+
+  return { updateDisplay, updateDayContainers };
 })();
 
 logic.fetchWeather('Sofia').then((data) => console.log(data));
--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,11 @@
 export const logic = (() => {
   const apiKey = 'A58FRBCCTVYQN9LT7Q79Z67WZ';
 
-  function fahrenheitToC(f) {
-    return ((f - 32) * 5) / 9;
-  }
+  // function fahrenheitToC(f) {
+  //   return ((f - 32) * 5) / 9;
+  // }
+
+  const fahrenheitToC = (f) => ((f - 32) * 5) / 9;
 
   const fetchWeather = async (city) => {
     try {
@@ -16,7 +18,6 @@ export const logic = (() => {
       }
 
       const data = await result.json();
-      console.log(data);
 
       return cleanData(data);
     } catch (err) {
@@ -26,9 +27,10 @@ export const logic = (() => {
 
   const cleanData = (data) => ({
     city: data.address,
-    currentTemp: Math.floor(fahrenheitToC(data.currentConditions.temp)),
+    currentTemp: Math.round(fahrenheitToC(data.currentConditions.temp)),
     desc: data.currentConditions.conditions,
     fiveDays: data.days.splice(0, 5),
   });
-  return { fetchWeather };
+
+  return { fetchWeather, fahrenheitToC };
 })();
